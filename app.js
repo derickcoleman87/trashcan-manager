b45b9c6dd96e179a117907d4d1077bb412ff6022
derickcoleman87 <107764740+derickcoleman87@users.noreply.github.com>
2023-06-10 14:38:05 -0400

    search function working!!

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 const data = [
-  { address: "10 Davis Rd. Bangor,Me 04401", trashCanId: 620336996 },
-  { address: "12 Davis Rd. Bangor,Me 04401", trashCanId: 620336993 },
-  { address: "55 Deer Isle Rd. Bangor,Me 04401", trashCanId: 620336991 },
+  { address: "10 Davis Rd. Bangor, Me 04401", trashCanId: "620336996" },
+  { address: "12 Davis Rd. Bangor, Me 04401", trashCanId: "620336993" },
+  { address: "55 Deer Isle Rd. Bangor, Me 04401", trashCanId: "620336991" },
 ];
-
-console.log(data);
-
-let input = document.querySelector("input");
-
-const searchButton = document.getElementById("search-btn");
-searchButton.addEventListener("click", search);
-
-function search() {
-  const newArr = [];
-  console.log("click");
-  console.log(input.value);
-  if (input.value.toLocaleLowerCase === data.address.toLocaleLowerCase) {
-    newArr.push(data.address);
-    console.log(newArr);
-  }
-}
-
 // const fs = require("fs");
 // const path = require("path");
 // const csv = require("fast-csv");
@@ -32,3 +14,36 @@ function search() {
 //   .on("error", (error) => console.error(error))
 //   .on("data", (row) => data.push(row))
 //   .on("end", (rowCount) => console.log(data));
+
+let input = document.querySelector("input");
+
+const searchButton = document.getElementById("search-btn");
+searchButton.addEventListener("click", search);
+
+function search() {
+  let newArr = [];
+
+  console.log("click");
+  console.log(typeof input.value);
+
+  for (let i = 0; i < data.length; i++) {
+    if (input.value === data[i].trashCanId || input.value === data[i].address) {
+      newArr.push(data[i]);
+      console.log(newArr);
+    }
+  }
+
+  for (let i = 0; i < newArr.length; i++) {
+    let results = document.createElement("div");
+    results.classList.add("results");
+
+    results.innerHTML = `
+      <h3>Address: ${newArr[i].address}</h3> 
+      <h3>Trashcan ID #: ${newArr[i].trashCanId}</h3> <button>Delete</button>
+      <button>Add</button>
+    `;
+    console.log(results);
+
+    document.querySelector(".results-container").append(results);
+  }
+}
