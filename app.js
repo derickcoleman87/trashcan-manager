7d7f6df7617dd3e5443aab071a2b2d50f320a19d
derickcoleman87 <107764740+derickcoleman87@users.noreply.github.com>
2024-03-30 06:23:46 -0400

    Updated Clear function

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const data = [
   { address: "10 Davis Rd. Bangor, Me 04401", trashCanId: "620336996" },
   { address: "12 Davis Rd. Bangor, Me 04401", trashCanId: "620336993" },
   { address: "55 Deer Isle Rd. Bangor, Me 04401", trashCanId: "620336991" },
+  { address: "35 Deer Isle Rd. Bangor, Me 04401", trashCanId: "620336997" },
+  { address: "23 Fort Knox Ave. Bangor, Me 04401", trashCanId: "620336983" },
+  { address: "605 Finson Road. Bangor, Me 04401", trashCanId: "620336987" },
+  { address: "27 Blue Hill East. Bangor, Me 04401", trashCanId: "620336990" },
 ];
 // const fs = require("fs");
 // const path = require("path");
@@ -23,12 +27,14 @@ searchButton.addEventListener("click", search);
 
 input.addEventListener("keyup", function (event) {
   if (event.key == "Enter") {
-    console.log("Enter");
+    // console.log("Enter");
     search();
   }
 });
 
 function search() {
+  clearResults();
+  console.log("Searching...");
   let newArr = [];
 
   for (let i = 0; i < data.length; i++) {
@@ -56,4 +62,12 @@ function search() {
 
     document.querySelector(".results-container").append(results);
   }
+}
+
+function clearResults() {
+  const resultsContainer = document.querySelector(".results-container");
+  while (resultsContainer.firstChild) {
+    resultsContainer.removeChild(resultsContainer.firstChild);
+  }
+  console.log("Clear results");
 }
